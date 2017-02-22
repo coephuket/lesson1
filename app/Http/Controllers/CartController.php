875ff6474e875f572b3b5afc1b63b032cae45c0f
Warodom <wwarodom@gmail.com>
2017-02-22 19:21:16 +0700

    cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,8 +8,21 @@ class CartController extends Controller
 {
 
 	public function index() {
-		session(['cakePrice' => '49.95']);
+		session(['cakePrice' => '49.95']); 
+		session()->put('foo','foobar');		
 		//var_dump( session('cakePrice') );
+		//session()->forget('foo'); //delete foo session		
+		session()->pull('foo');		//delete foo session
+		// session()->flush();		// delete all sessions
+		
+		$teams = ["liverpool","manU"];
+		session()->push('teams',$teams);
+
+		$fruit = [ 'fruit' => 'apple', 'veggie' => 'carrot' ];
+		session()->put('fruit',$fruit);
+		session()->put('fruit.kaoy','Banana');
+		
+		dd( session()->all() );
 		return view('cart.index');
 	}
 
