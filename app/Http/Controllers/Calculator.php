f6aa7cbce83ebd4ab08ea9f655bc93b6f53d0623
Warodom <wwarodom@gmail.com>
2017-03-01 19:15:45 +0700

    validate 1

--- app/Http/Controllers/Calculator.php
+++ app/Http/Controllers/Calculator.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class Calculator extends Controller
 {
@@ -11,15 +12,18 @@ class Calculator extends Controller
     }
 
     public function submit(Request $request) {
-    	$num1 = $request->input('num1');
-    	$num2 = $request->input('num2');
 
-        if ( $num1 == '' || $num2 == '')
-            $error = 'มีข้อผิดพลาด'; 
-        else       
-            $result = $num1 + $num2;
-         return view('validate.cal',
-              compact('num1','num2','result','error'));
+        $rules = [
+            'num1'  => 'required|numeric'
+        ];
+        $validator = Validator::make($request->all(),$rules);
+        dd ($validator->passes() );
+
+    	// $num1 = $request->input('num1');
+    	// $num2 = $request->input('num2');        
+     //    $result = $num1 + $num2;
+     //    return view('validate.cal',
+     //          compact('num1','num2','result'));
 
     }
 }
