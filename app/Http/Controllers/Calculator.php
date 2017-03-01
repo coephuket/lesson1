d84d0e6f6c0d4f7489956e334c439f93a13d6379
Warodom <wwarodom@gmail.com>
2017-03-01 18:49:14 +0700

    validate 1

--- app/Http/Controllers/Calculator.php
+++ app/Http/Controllers/Calculator.php
@@ -7,18 +7,20 @@ use Illuminate\Http\Request;
 class Calculator extends Controller
 {
     public function index() {
-        $num1="0";
-        $num2="0";
-        $result="0";
-        return view('validate.cal',
-                compact('num1','num2','result'));
+        return view('validate.cal');
     }
 
     public function submit(Request $request) {
     	$num1 = $request->input('num1');
     	$num2 = $request->input('num2');
-    	$result = $num1 + $num2;
-    	return view('validate.cal',
-    			compact('num1','num2','result'));
+
+        if ( $num1 == '' || $num2 == '')
+            $error = 'มีข้อผิดพลาด';
+        else 
+            $error = '';        
+        $result = $num1 + $num2;
+         return view('validate.cal',
+              compact('num1','num2','result','error'));
+
     }
 }
--- resources/views/validate/cal.blade.php
+++ resources/views/validate/cal.blade.php
@@ -3,11 +3,16 @@
 @section('title','Calculator')
 
 @section('content')
+
+	<div class="alert alert-danger">
+		{{ $error or ''}}
+	</div>
+
 	<form action="/cal" method="post">
 		<label for="num1">Num:1</label>
-		<input type="number" name="num1" value="{{$num1 or ''}}" required> + <br>
+		<input type="text" name="num1" value="{{$num1 or ''}}"> + <br>
 		<label for="num2">Num:2</label>
-		<input type="number" name="num2" value="{{$num2 or ''}}" required><br>
+		<input type="text" name="num2" value="{{$num2 or ''}}"><br>
 		<label for="Result">Result:</label>
 		<input type="number" name="result" value="{{$result or ''}}">
 		{{csrf_field()}}
