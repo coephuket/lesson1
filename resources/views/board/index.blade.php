6a8ac579a89dfe73fdda711e21ac330acdb2391f
Warodom <wwarodom@gmail.com>
2017-04-05 19:04:39 +0700

    Show

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -3,13 +3,15 @@
 
 @section('content')	
 	@foreach($boards as $index => $item)
-		{{$index+1}}: {{$item->title}} : {{$item->body}} <br>
-		<a href="/boards/{{$item->id}}">Show</a> |
-		<a href="/boards/{{$item->id}}/edit">Edit</a> <br>
-		<form action="/boards/{{$item->id}}" method="post">
+		<h4>{{$index+1}}: {{$item->title}} </h4>
+		<p>{{$item->body}} </p>
+
+		<form action="/boards/{{$item->id}}" method="post" class="form-inline">
+			<a href="/boards/{{$item->id}}" class="btn btn-info">Show</a> 
+			<a href="/boards/{{$item->id}}/edit" class="btn btn-default">Edit</a> 
 			<input type="hidden" name="_method" value="DELETE" />
 			{{csrf_field()}}
-			<button>Delete</button>
+			<button class="btn btn-primary">Delete</button>
 		</form>
 		<br>
 	@endforeach
